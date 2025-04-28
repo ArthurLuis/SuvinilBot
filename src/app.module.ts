e72963fdc039111aa635b5db6f437e6b83ca052c
ArthurLuis <alfa@cin.ufpe.br>
2025-04-28 14:46:29 -0300

    feat: embedding services created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { DatabaseModule } from './database/database.module';
 import { TintasModule } from './tintas/tintas.module';
 import { OpenaiModule } from './openai/openai.module';
 import { ConfigModule } from '@nestjs/config';
+import { EmbeddingService } from './embedding/embedding.service';
 
 @Module({
   imports: [DatabaseModule, TintasModule, OpenaiModule, ConfigModule.forRoot()],
+  providers: [EmbeddingService],
 })
 export class AppModule {}
