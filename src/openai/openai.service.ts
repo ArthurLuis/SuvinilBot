ae81959f16c683646c8cd72b6ed5920975f3cc07
ArthurLuis <alfa@cin.ufpe.br>
2025-04-30 17:56:07 -0300

    feat: new service and change to gpt-4.0

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
 
-const MODEL = 'gpt-3.5-turbo';
+const MODEL = 'gpt-4-turbo';
 const TEMPERATURE = 0.7;
 const INSTRUCTIONS = `You are a helpful assistant. You will be given a question and you will answer it in the best way possible. If you don't know the answer, say "I don't know".`;
 
@@ -39,5 +39,23 @@ export class OpenaiService {
       input,
     });
     return embedding;
+  }
+
+  async getLightResponse(text: string) {
+    const chatCompletion = await this.openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: INSTRUCTIONS,
+        },
+        {
+          role: 'user',
+          content: text,
+        },
+      ],
+      temperature: TEMPERATURE,
+    });
+    return chatCompletion.choices[0].message.content;
   }
 }
